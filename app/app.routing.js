66e43a819b1de0b2ad68aee470b8d5b954bd7a6c
Vijayarajamayuri <avrmayuri74@gmail.com>
2019-05-22 18:43:56 -0500

    commited

--- app/app.routing.js
+++ app/app.routing.js
@@ -14,4 +14,8 @@ angular.module("myApp").config(function ($routeProvider) {
         templateUrl : "app/components/pmbok/pmbokmatrix.html",
         controller : "matrixctrl"
     });
+    .when("/aboutme",{
+        templateUrl :"app/components/aboutme/index.html",
+        controller :"Student"
+    });
 });
